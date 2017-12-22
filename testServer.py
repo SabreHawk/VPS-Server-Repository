90ae347b9922f56a65916383036c22dc13c69846
Zhiquan.Wang <mySabreHawk@outlook.com>
2017-12-22 11:36:04 +0800

    Update Server End Code 2017/12/22 - 11:36

--- testServer.py
+++ testServer.py
@@ -7,9 +7,12 @@ serversocket.bind((host_ip, port))
 # 设置最大连接数，超过后排队
 serversocket.listen(5)
 msg = "received your request Sabrehawk"
-while True:
+whihe True:
     # 建立客户端连接
     (clientsocket,addr) = serversocket.accept()      
     clientsocket.send(msg.encode('utf-8'))
     print("client ip" ,clientsocket.recv(1024).decode('utf-8'))
     clientsocket.close()
+
+print("Server End")
+
